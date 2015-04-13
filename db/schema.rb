65ff96346f916365c8b96c763cd2968c01ebccc4
Tall Paul <pmd6624@rit.edu>
2015-04-13 16:46:27 -0400

    added answers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413204239) do
+ActiveRecord::Schema.define(version: 20150413204320) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
